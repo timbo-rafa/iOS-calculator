97a765d59a3c4f0835e0723273329e3fceb7e0d7
Student <student@SETASs-Mac-mini-2.local>
2017-09-22 22:25:31 -0400

    Displaying operations (bug after x)

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -3,7 +3,7 @@
  * Date: September 19, 2017
  * StudentID: 300962878
  * Description: Calculator App for iOS
- * Version 0.4 - Saving number as double
+ * Version 0.5 - Displaying operations (bug after x)
  *
  */
 
@@ -12,13 +12,38 @@ import UIKit
 class ViewController: UIViewController {
 
     var numberOnDisplay:Double = 0;
+    var performingMath = true;
+    
     @IBOutlet weak var labelDisplay: UILabel!
     
     @IBAction func numbers(_ sender: UIButton) {
-      labelDisplay.text = labelDisplay.text! + String(sender.tag)
+      if performingMath == true {
+        labelDisplay.text = String(sender.tag)
+        performingMath = false
+      }
+      else {
+        labelDisplay.text = labelDisplay.text! + String(sender.tag)
+      }
       numberOnDisplay = Double(labelDisplay.text!)!
     }
     
+    @IBAction func operations(_ sender: UIButton) {
+        if labelDisplay.text != "" {
+          if (sender.tag == 14) {
+            labelDisplay.text = "/"
+          }
+          if (sender.tag == 13) {
+            labelDisplay.text = "x"
+          }
+          if (sender.tag == 12) {
+            labelDisplay.text = "-"
+          }
+          if (sender.tag == 11) {
+            labelDisplay.text = "+"
+          }
+        }
+    
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()

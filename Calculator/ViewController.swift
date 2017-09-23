d927c3e0ca8c062a741fc9de72031258efaaa1ee
Student <student@SETASs-Mac-mini-2.local>
2017-09-22 21:45:56 -0400

    Displaying number keypresses

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,15 +1,23 @@
-//
-//  ViewController.swift
-//  Calculator
-//
-//  Created by Student on 2017-09-19.
-//  Copyright © 2017 Centennial College. All rights reserved.
-//
+/*
+ * Name: Rafael Timbo Matos
+ * Date: September 19, 2017
+ * StudentID: 300962878
+ * Description: Calculator App for iOS
+ * Version 0.3 - Displaying number keypresses
+ *
+ */
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelDisplay: UILabel!
+    
+    @IBAction func numbers(_ sender: UIButton) {
+      labelDisplay.text = labelDisplay.text! + String(sender.tag)
+    
+    
+    }
     
     
     override func viewDidLoad() {

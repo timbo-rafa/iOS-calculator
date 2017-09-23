b7b5f5a692854f55a7550979c005583bf0dfa11f
Student <pv.ryan14@gmail.com>
2017-09-23 14:06:02 -0400

    Operations Implemented. Clear functionality. Equal implementation.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -3,7 +3,7 @@
  * Date: September 19, 2017
  * StudentID: 300962878
  * Description: Calculator App for iOS
- * Version 0.5 - Displaying operations (bug after x)
+ * Version 1.0 - Operations Implemented. Clear functionality. Equal implementation. 
  *
  */
 
@@ -12,37 +12,65 @@ import UIKit
 class ViewController: UIViewController {
 
     var numberOnDisplay:Double = 0;
+    var previousNumber:Double = 0;
     var performingMath = true;
+    var operation = 0;
     
-    @IBOutlet weak var labelDisplay: UILabel!
+    @IBOutlet weak var label: UILabel!
     
     @IBAction func numbers(_ sender: UIButton) {
       if performingMath == true {
-        labelDisplay.text = String(sender.tag)
+        label.text = String(sender.tag)
         performingMath = false
       }
       else {
-        labelDisplay.text = labelDisplay.text! + String(sender.tag)
+        label.text = label.text! + String(sender.tag)
       }
-      numberOnDisplay = Double(labelDisplay.text!)!
+      numberOnDisplay = Double(label.text!)!
     }
     
     @IBAction func operations(_ sender: UIButton) {
-        if labelDisplay.text != "" {
+        previousNumber = Double(label.text!)!
+        if label.text != "" {
           if (sender.tag == 14) {
-            labelDisplay.text = "/"
+            label.text = "/"
           }
           if (sender.tag == 13) {
-            labelDisplay.text = "x"
+            label.text = "*"
           }
           if (sender.tag == 12) {
-            labelDisplay.text = "-"
+            label.text = "-"
           }
           if (sender.tag == 11) {
-            labelDisplay.text = "+"
+            label.text = "+"
           }
         }
+        
+        operation = sender.tag
+        performingMath = true
     
+    }
+    
+    @IBAction func equals(_ sender: UIButton) {
+        if operation == 14 {
+            label.text = String(previousNumber / numberOnDisplay)
+        }
+        else if operation == 13 {
+            label.text = String(previousNumber * numberOnDisplay)
+        }
+        else if operation == 12 {
+            label.text = String(previousNumber - numberOnDisplay)
+        }
+        else if operation == 11 {
+            label.text = String(previousNumber + numberOnDisplay)
+        }
+    }
+    
+    @IBAction func clear(_ sender: UIButton) {
+        label.text = "0"
+        previousNumber = 0
+        numberOnDisplay = 0
+        operation = 0
     }
     
     override func viewDidLoad() {

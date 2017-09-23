6b40e92f6b4b9524a43f9f881867dbc041531b9e
timbo-rafa <timbo.rafa@gmail.com>
2017-09-23 17:06:02 -0400

    Documenting Code. Removed redundant code.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,9 +1,14 @@
-/*
+/* ViewController.swift
  * Name: Rafael Timbo Matos
  * Date: September 19, 2017
  * StudentID: 300962878
  * Description: Calculator App for iOS
- * Version 3.0 - Start with 0, by user experience
+ * based on https://www.youtube.com/watch?v=AG2QDwmj64A
+ * Version 3.1 - Documenting Code. Removed redundant code.
+ *
+ * Tests:
+ * OK 5*= keeps multiplying by 5 by 5 by 5...
+ * OK 2+= keeps summing 2
  * OK press 0...... stays 0.
  * OK press 0.5..... stays 0.5
  * OK press 0 stays 0
@@ -16,20 +21,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // VARIABLES =====================================
+    // Double value for number on screen
     var numberOnDisplay:Double = 0;
+    // previous number typed or calculated
     var previousNumber:Double = 0;
+    // variable that tells us if an operation was the last keypress
     var performingMath = true;
+    // type of operation (related to button tag)
     var operation = 0;
     
+    // label displayed on screen
     @IBOutlet weak var label: UILabel!
     
+    // METHODS ==========================================
+    // number keypress handler
     @IBAction func numbers(_ sender: UIButton) {
+      // if an operation was the last key pressed, display should be wiped
       if performingMath == true {
         label.text = String(sender.tag)
         performingMath = false
       }
       else {
+        // prevent 0 spam on display (00000)
         if label.text == "0" {
           label.text = String(sender.tag)
         } else {
@@ -37,27 +52,23 @@ class ViewController: UIViewController {
         }
       }
       numberOnDisplay = Double(label.text!)!
-        // Prevent 0 spamming on screen
-        if label.text == "00" {
-            label.text = "0"
-        }
     }
-    
+    // handle plus (+), minus (-), multiply (*), and division (/) buttons
     @IBAction func operations(_ sender: UIButton) {
         if performingMath == false {
           previousNumber = Double(label.text!)!
         }
         if label.text != "" { //????
-          if (sender.tag == 14) {
+          if (sender.tag == 14) { // DIVIDE
             label.text = "/"
           }
-          if (sender.tag == 13) {
+          if (sender.tag == 13) { // MULTIPLY
             label.text = "*"
           }
-          if (sender.tag == 12) {
+          if (sender.tag == 12) { // MINUS
             label.text = "-"
           }
-          if (sender.tag == 11) {
+          if (sender.tag == 11) { // PLUS
             label.text = "+"
           }
         }
@@ -67,7 +78,9 @@ class ViewController: UIViewController {
     
     }
     
+    // Equal keypress handler
     @IBAction func equals(_ sender: UIButton) {
+        // perform operation requested
         if operation == 14 { // DIVIDE
             label.text = String(previousNumber / numberOnDisplay)
         }
@@ -84,6 +97,7 @@ class ViewController: UIViewController {
         previousNumber = Double(label.text!)!
     }
     
+    // C (clear) keypress handler
     @IBAction func clear(_ sender: UIButton) {
         label.text = "0"
         previousNumber = 0
@@ -91,13 +105,13 @@ class ViewController: UIViewController {
         operation = 0
     }
     
+    // decimal point (.) keypress handler
     @IBAction func decimal(_ sender: UIButton) {
       let dotCharacterSet = CharacterSet(charactersIn: "." )
+      // prevent multiple occurrences
       if label.text?.rangeOfCharacter(from: dotCharacterSet ) == nil {
-        if label.text == "" {
-          label.text = "0."
-        }
-        else if performingMath == true {
+        // prepend 0 if necessary
+        if performingMath == true {
           label.text = "0."
           performingMath = false
         }
@@ -108,11 +122,13 @@ class ViewController: UIViewController {
       
     }
     
+    // change sign (+-) keypress handler
     @IBAction func changeSign(_ sender: UIButton) {
         previousNumber = Double( label.text!)! * -1
         label.text = String( previousNumber )
     }
     
+    // percent (%) keypress handler
     @IBAction func percent(_ sender: UIButton) {
         previousNumber = Double(label.text!)! / 100
         label.text = "%"
@@ -120,10 +136,10 @@ class ViewController: UIViewController {
         performingMath = true
     }
     
-    
+    // init
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Start with 0, by user experience.
+        // Start displaying 0, by user experience.
         label.text = "0"
     }
 
@@ -131,7 +147,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 

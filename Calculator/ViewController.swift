65d39247adaa1dae90ce25c2aefcac06229e2106
Rafael Timbo <timbo.rafa@gmail.com>
2017-09-25 18:30:33 -0400

    Minor Design Changes. Dropped animation.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -4,7 +4,7 @@
  * StudentID: 300962878
  * Description: Calculator App for iOS
  * based on https://www.youtube.com/watch?v=AG2QDwmj64A
- * Version 5.0 - Round Corner Buttons.
+ * Version 5.1 - Minor design changes. Changed color of percent and changeSign buttons
  *
  * Tests:
  * OK 5*= keeps multiplying by 5 by 5 by 5...
@@ -23,105 +23,21 @@ import UIKit
 class ViewController: UIViewController {
     
     // VARIABLES =====================================
+    
+    
+    
     // Double value for number on screen
     var numberOnDisplay:Double = 0;
     // previous number typed or calculated
     var previousNumber:Double = 0;
-    // variable that tells us if an operation was the last keypress
-    var performingMath = true;
+    // variable that tells us the operation that was the last requested
+    @IBOutlet weak var performingMath: UIButton!
     // type of operation (related to button's tag)
     var operation = 0;
     
     // label displayed on screen
     @IBOutlet weak var label: UILabel!
-    
-    // METHODS ==========================================
-    // number keypress handler
-    @IBAction func numbers(_ sender: UIButton) {
-      // if an operation was the last key pressed, display should be wiped
-      if performingMath == true {
-        label.text = String(sender.tag)
-        performingMath = false
-      }
-      else {
-        // prevent 0 spam on display (00000)
-        if label.text == "0" {
-          label.text = String(sender.tag)
-        } else {
-          label.text = label.text! + String(sender.tag)
-        }
-      }
-      numberOnDisplay = Double(label.text!)!
-    }
-    // handle plus (+), minus (-), multiply (*), and division (/) buttons
-    @IBAction func operations(_ sender: UIButton) {
-        if performingMath == false {
-          previousNumber = Double(label.text!)!
-        }
-        if label.text != "" { //????
-          if (sender.tag == 14) { // DIVIDE
-            label.text = "/"
-          }
-          if (sender.tag == 13) { // MULTIPLY
-            label.text = "*"
-          }
-          if (sender.tag == 12) { // MINUS
-            label.text = "-"
-          }
-          if (sender.tag == 11) { // PLUS
-            label.text = "+"
-          }
-        }
-        
-        operation = sender.tag
-        performingMath = true
-    
-    }
-    
-    // Equal keypress handler
-    @IBAction func equals(_ sender: UIButton) {
-        // perform operation requested
-        if operation == 14 { // DIVIDE
-            label.text = String(previousNumber / numberOnDisplay)
-        }
-        else if operation == 13 { // MULTIPLY
-            label.text = String(previousNumber * numberOnDisplay)
-        }
-        else if operation == 12 { // MINUS
-            label.text = String(previousNumber - numberOnDisplay)
-        }
-        else if operation == 11 { // PLUS
-            label.text = String(previousNumber + numberOnDisplay)
-        }
-        performingMath = true
-        previousNumber = Double(label.text!)!
-    }
-    
-    // C (clear) keypress handler
-    @IBAction func clear(_ sender: UIButton) {
-        label.text = "0"
-        previousNumber = 0
-        numberOnDisplay = 0
-        operation = 0
-    }
-    
-    // decimal point (.) keypress handler
-    @IBAction func decimal(_ sender: UIButton) {
-      let dotCharacterSet = CharacterSet(charactersIn: "." )
-      // prevent multiple occurrences
-      if label.text?.rangeOfCharacter(from: dotCharacterSet ) == nil {
-        // prepend 0 if necessary
-        if performingMath == true {
-          label.text = "0."
-          performingMath = false
-        }
-        else {
-          label.text = label.text! + "."
-        }
-      }
-      
-    }
- 
+    // Buttons
     @IBOutlet weak var zero: UIButton!
     @IBOutlet weak var decimalPoint: UIButton!
     @IBOutlet weak var equal: UIButton!
@@ -143,6 +59,91 @@ class ViewController: UIViewController {
     @IBOutlet weak var nine: UIButton!
     @IBOutlet weak var voidButton: UIButton!
     
+    // METHODS ==========================================
+    // number keypress handler
+    @IBAction func numbers(_ sender: UIButton) {
+      // if an operation was the last key pressed, display should be wiped
+      if performingMath != nil {
+        label.text = String(sender.tag)
+        performingMath = nil
+      }
+      else {
+        // prevent 0 spam on display (00000)
+        if label.text == "0" {
+          label.text = String(sender.tag)
+        } else {
+          label.text = label.text! + String(sender.tag)
+        }
+      }
+      numberOnDisplay = Double(label.text!)!
+    }
+    // handle plus (+), minus (-), multiply (*), and division (/) buttons
+    @IBAction func operations(_ sender: UIButton) {
+        if performingMath == nil {
+          previousNumber = Double(label.text!)!
+        }
+        if label.text != "" { //????
+          if (sender.tag == 14) { // DIVIDE
+            label.text = "/"
+          }
+          if (sender.tag == 13) { // MULTIPLY
+            label.text = "*"
+          }
+          if (sender.tag == 12) { // MINUS
+            label.text = "-"
+          }
+          if (sender.tag == 11) { // PLUS
+            label.text = "+"
+          }
+        }
+        operation = sender.tag
+        performingMath = sender
+    }
+    
+    // Equal keypress handler
+    @IBAction func equals(_ sender: UIButton) {
+        // perform operation requested
+        if operation == 14 { // DIVIDE
+            label.text = String(previousNumber / numberOnDisplay)
+        }
+        else if operation == 13 { // MULTIPLY
+            label.text = String(previousNumber * numberOnDisplay)
+        }
+        else if operation == 12 { // MINUS
+            label.text = String(previousNumber - numberOnDisplay)
+        }
+        else if operation == 11 { // PLUS
+            label.text = String(previousNumber + numberOnDisplay)
+        }
+        performingMath = sender
+        previousNumber = Double(label.text!)!
+    }
+    
+    // C (clear) keypress handler
+    @IBAction func clear(_ sender: UIButton) {
+        label.text = "0"
+        previousNumber = 0
+        numberOnDisplay = 0
+        operation = 0
+    }
+    
+    // decimal point (.) keypress handler
+    @IBAction func decimal(_ sender: UIButton) {
+      let dotCharacterSet = CharacterSet(charactersIn: "." )
+      // prevent multiple occurrences
+      if label.text?.rangeOfCharacter(from: dotCharacterSet ) == nil {
+        // prepend 0 if necessary
+        if performingMath != nil {
+          label.text = "0."
+          performingMath = nil
+        }
+        else {
+          label.text = label.text! + "."
+        }
+      }
+      
+    }
+    
     // change sign (+-) keypress handler
     @IBAction func changeSign(_ sender: UIButton) {
         previousNumber = Double( label.text!)! * -1
@@ -151,15 +152,12 @@ class ViewController: UIViewController {
     
     // percent (%) keypress handler
     @IBAction func percent(_ sender: UIButton) {
-        previousNumber = Double(label.text!)! / 100
-        label.text = "%"
-        operation = 13 // Multiply
-        performingMath = true
-    }
-    
-    // ANIMATION
-    @IBAction func animate(_ sender: UIButton) {
-      
+        if (label.text != "%") {
+            previousNumber = Double(label.text!)! / 100
+            label.text = "%"
+            operation = 13 // Multiply
+            performingMath = sender
+        }
     }
     
     // init
@@ -170,6 +168,7 @@ class ViewController: UIViewController {
         label.text = "0"
     }
     
+    // round button corners
     func roundButtonCorners() {
         zero.layer.cornerRadius = 5
         one.layer.cornerRadius = 5
